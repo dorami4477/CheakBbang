c1ec4290601fbbf7a52bb5da16a7f06ef0a4dd4b
dahyun <dahyun4477@gmail.com>
2024-09-10 17:14:40 +0900

    Initial Commit

---
+++ CheakBbang/CheakBbangApp.swift
@@ -0,0 +1,17 @@
+//
+//  CheakBbangApp.swift
+//  CheakBbang
+//
+//  Created by 박다현 on 9/10/24.
+//
+
+import SwiftUI
+
+@main
+struct CheakBbangApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
