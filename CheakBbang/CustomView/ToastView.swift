ac696ce726aa523f1d48dd6bf6597463ee6b04b4
dahyun <dahyun4477@gmail.com>
2024-09-29 00:38:24 +0900

    불필요한 라이브러리 삭제 ( sdwebimage )

---
+++ CheakBbang/CustomView/ToastView.swift
@@ -0,0 +1,8 @@
+//
+//  ToastView.swift
+//  CheakBbang
+//
+//  Created by 박다현 on 9/29/24.
+//
+
+import Foundation
---
+++ CheakBbang/Model/Toast.swift
@@ -0,0 +1,8 @@
+//
+//  Toast.swift
+//  CheakBbang
+//
+//  Created by 박다현 on 9/29/24.
+//
+
+import Foundation
