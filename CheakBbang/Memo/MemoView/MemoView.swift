e95841c12196e464f00aeb31c648f55b47bc3550
dahyun <dahyun4477@gmail.com>
2024-10-26 17:31:26 +0900

    Fix: Data(memo -> viewModel) 변경

--- CheakBbang/Memo/MemoView/MemoView.swift
+++ CheakBbang/Memo/MemoView/MemoView.swift
@@ -59,7 +59,7 @@ struct MemoView: View {
         .toolbar {
             ToolbarItemGroup(placement: .topBarTrailing) {
                 NavigationLink {
-                    NavigationLazyView(AddMemoView(viewModel: AddMemoViewModel(repository: MyMemoRepository()), item: MyBook().toMyBookDTO(), memo: memo))
+                    NavigationLazyView(AddMemoView(viewModel: AddMemoViewModel(repository: MyMemoRepository()), item: MyBook().toMyBookDTO(), memo: viewModel.output.memo))
                 } label: {
                     ImageWrapper(name: ImageName.edit)
                         .frame(width: 24, height: 24)
