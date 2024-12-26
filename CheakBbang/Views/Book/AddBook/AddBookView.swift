f75ec12034ff6abb87a0a3f5f4a365113f13ed3b
dahyun <dahyun4477@gmail.com>
2024-12-26 18:24:53 +0900

    refactor: View 의 요소 Section으로 분리

--- CheakBbang/Views/Book/AddBook/AddBookView.swift
+++ CheakBbang/Views/Book/AddBook/AddBookView.swift
@@ -12,62 +12,28 @@ struct AddBookView: View {
     @StateObject var viewModel: AddBookViewModel
     //@EnvironmentObject var appState: AppState
     //@State private var isSaved = false
-    var item: Item
+    var item: ItemDTO
     @Binding var addNew: Bool
     
     
     var body: some View {
         VStack {
-            Text(viewModel.output.bookItem.title)
-                .multilineTextAlignment(.center)
-                .bold()
-                .font(.system(size: 21))
-                .padding(.bottom, 10)
-            
-            Text(viewModel.output.bookItem.subInfo.originalTitle ?? "")
-                .multilineTextAlignment(.center)
-                .font(.system(size: 17))
+            titleSection(item: viewModel.output.bookItem)
             
             Divider()
                 .padding(.vertical)
             
-            Text("상태")
-                .foregroundStyle(.gray)
-                .font(.system(size: 14))
-            HStack {
-                radioSectionGroup(sectionTitle: "readingState", selectedItem: $viewModel.input.readingState, selectedColor: .accent) { index, state in
-                    viewModel.input.readingState = state
-                }
-            }
+            stateSection()
             
             Divider()
                 .padding(.vertical)
             
-            Text(viewModel.input.readingState == .upcoming ? "내 기대" : "내 평점")
-                .foregroundStyle(.gray)
-                .font(.system(size: 14))
-            RratingHeartView(rating: $viewModel.input.rating)
+            ratingSection()
             
             Divider()
                 .padding(.vertical)
             
-            if viewModel.input.readingState == .finished || viewModel.input.readingState == .ongoing {
-                Text("독서 시작일")
-                    .foregroundStyle(.gray)
-                    .font(.system(size: 14))
-                DatePicker(selection: $viewModel.input.startDate, displayedComponents: .date) {}
-                    .labelsHidden()
-                    .padding(.bottom, 10)
-            }
-
-            
-            if viewModel.input.readingState == .finished {
-                Text("독서 종료일")
-                    .foregroundStyle(.gray)
-                    .font(.system(size: 14))
-                DatePicker(selection: $viewModel.input.endDate, in: viewModel.input.startDate..., displayedComponents: .date) {}
-                    .labelsHidden()
-            }
+            dateSection()
             
             Spacer()
             
@@ -77,7 +43,6 @@ struct AddBookView: View {
                     viewModel.action(.addButtonTap)
                     addNew = true
                     dismiss()
-                    //isSaved = true
                 }
 
         }
@@ -86,6 +51,66 @@ struct AddBookView: View {
             viewModel.action(.viewOnTask(item: item))
         }
 
+    }
+    
+    func titleSection(item: ItemDTO) -> some View {
+        VStack {
+            Text(item.title)
+                .multilineTextAlignment(.center)
+                .bold()
+                .font(.system(size: 21))
+                .padding(.bottom, 10)
+            
+            Text(item.subInfo.originalTitle ?? "")
+                .multilineTextAlignment(.center)
+                .font(.system(size: 17))
+        }
+    }
+    
+    func stateSection() -> some View {
+        VStack {
+            Text("상태")
+                .foregroundStyle(.gray)
+                .font(.system(size: 14))
+            
+            HStack {
+                radioSectionGroup(sectionTitle: "readingState", selectedItem: $viewModel.input.readingState, selectedColor: .accent) { index, state in
+                    viewModel.input.readingState = state
+                }
+            }
+        }
+    }
+    
+    func ratingSection() -> some View {
+        VStack {
+            Text(viewModel.input.readingState == .upcoming ? "내 기대" : "내 평점")
+                .foregroundStyle(.gray)
+                .font(.system(size: 14))
+            
+            RratingHeartView(rating: $viewModel.input.rating)
+        }
+    }
+    
+    func dateSection() -> some View {
+        VStack {
+            if viewModel.input.readingState == .finished || viewModel.input.readingState == .ongoing {
+                Text("독서 시작일")
+                    .foregroundStyle(.gray)
+                    .font(.system(size: 14))
+                DatePicker(selection: $viewModel.input.startDate, displayedComponents: .date) {}
+                    .labelsHidden()
+                    .padding(.bottom, 10)
+            }
+            
+            
+            if viewModel.input.readingState == .finished {
+                Text("독서 종료일")
+                    .foregroundStyle(.gray)
+                    .font(.system(size: 14))
+                DatePicker(selection: $viewModel.input.endDate, in: viewModel.input.startDate..., displayedComponents: .date) {}
+                    .labelsHidden()
+            }
+        }
     }
 }
 
