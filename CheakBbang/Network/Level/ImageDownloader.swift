444f120c0cf9cc5e8649c1ebf0fb541e59b67995
dahyun <dahyun4477@gmail.com>
2025-02-20 22:35:04 +0900

    refactor: withTaskGroup의 group.addTask시점에 파일매니저에 이미지 저장되도록 변경(메모리 효율성 관점에서 개선)

--- CheakBbang/Network/Level/ImageDownloader.swift
+++ CheakBbang/Network/Level/ImageDownloader.swift
@@ -12,7 +12,7 @@ final class ImageDownloader: NetworkRequestConvertible {
     
     // MARK: - CatBookListView의 toy Image용
     func loadImages(for level: Int) async {
-        await withTaskGroup(of: (Int, Data?).self) { group in
+        await withTaskGroup(of: (Int, UIImage?).self) { group in
             for i in 1...level {
                 if PhotoFileManager.shared.loadFileURL(filename: "toy_\(i)") != nil {
                     continue
@@ -20,13 +20,13 @@ final class ImageDownloader: NetworkRequestConvertible {
                 
                 group.addTask {
                     let imageData = try? await self.callRequest(api: .toyImage(filename: "toy_\(i).png"), etagKey: "toy_\(i)")
-                    return (i - 1, imageData)
-                }
-            }
-            
-            for await (index, result) in group {
-                if let imageData = result, let image = UIImage(data: imageData) {
-                    PhotoFileManager.shared.saveImageToDocument(image: image, filename: "toy_\(index + 1)")
+                    if let imageData, let image = UIImage(data: imageData) {
+                        PhotoFileManager.shared.saveImageToDocument(image: image, filename: "toy_\(i)", isPNG: true)
+                        
+                        return (i - 1, image)
+                    } else {
+                        return (i - 1, nil)
+                    }
                 }
             }
         }
@@ -36,7 +36,7 @@ final class ImageDownloader: NetworkRequestConvertible {
     func loadImages(for level: [LevelModel]) async -> [UIImage?] {
         var results = [UIImage?](repeating: nil, count: level.count)
         
-        await withTaskGroup(of: (Int, Data?).self) { group in
+        await withTaskGroup(of: (Int, UIImage?).self) { group in
             for i in 0..<level.count {
                 let urlString = level[i].cover
                 let fileName = urlString.extractPathWithoutExtension()
@@ -48,20 +48,21 @@ final class ImageDownloader: NetworkRequestConvertible {
                 
                 group.addTask {
                     let imageData = try? await self.callRequest(api: .toyImage(filename: "\(fileName).png"), etagKey: fileName)
-                    return (i, imageData)
+                    if let imageData, let image = UIImage(data: imageData) {
+                        PhotoFileManager.shared.saveImageToDocument(image: image, filename: fileName)
+                        
+                        return (i, image)
+                    } else {
+                        return (i, nil)
+                    }
                 }
             }
             
-            for await (index, result) in group {
-                if let imageData = result, let image = UIImage(data: imageData) {
-                    results[index] = image
-                    
-                    let name = level[index].cover.extractPathWithoutExtension()
-                    PhotoFileManager.shared.saveImageToDocument(image: image, filename: name)
-                }
+            for await (index, image) in group {
+                results[index] = image
             }
         }
-
+        
         return results
     }
 }
